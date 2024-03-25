efc6473bb6ed5eac048e4c95ca6424be34dec997
GuilhermeMVasquez <guilhermemalgarizi@gmail.com>
2024-03-25 20:26:44 -0300

    change product: + Product factory

---
+++ ProductFactory.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class ProductFactory {
+    public ProductFactory() {
+    }
+
+    public IProduct createProduct(int id, String name, double price) {
+        return new Product(id, name, price);
+    }
+
+    public IProduct createProduct(int id, String name, List<IProduct> products) {
+        return new Combo(id, name, products);
+    }
+}
